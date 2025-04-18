974db1726e44603113b1a5fe3a2db4f8c06745af
yixi <gohyixi@gmail.com>
2025-04-19 01:06:20 +0800

    add approving withdrawal function in manager

--- code/utils/FileLoader.java
+++ code/utils/FileLoader.java
@@ -136,7 +136,7 @@ public class FileLoader implements IFileLoader {
                     LocalDateTime.parse(data[4], formatter), // Timestamp
                     data[5],                  // ReplierUserID
                     data[6],                  // ReplyContent
-                    LocalDateTime.parse(data[7], formatter)  // ReplierTimestamp
+                    data[7].equals("null") ? null : LocalDateTime.parse(data[7], formatter) // ReplierTimestamp
                 ));
             }
         } catch (IOException e) {
